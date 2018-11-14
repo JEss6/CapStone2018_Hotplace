a931596b12b62519545f151730706d3c123f0534
Choul <rlarla423@naver.com>
2018-11-14 11:57:38 +0900

    OPENCV_TEST
    OPENCV_TEST

---
+++ plz/Assets/Scripts/GoToFurniture.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GoToFurniture : MonoBehaviour {
+
+    public void GotoFurniture()
+    {
+        SceneManager.LoadScene(3);
+    }
+    
+}
